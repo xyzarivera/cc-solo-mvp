9ec5836a2720b21f39565b179b8f1cf946cff76d
Xyza Rivera <xyzarivera@gmail.com>
2021-10-21 22:47:38 +0800

    create create and read functions

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,112 @@
 const functions = require("firebase-functions");
+const logger = functions.logger;
 
 // The Firebase Admin SDK to access Firestore.
 const admin = require("firebase-admin");
+const { deepCopy } = require("@firebase/util");
 admin.initializeApp();
 
 exports.helloWorld = functions
-    .region("asia-east2")
-    .https.onCall((payload, context) => {
-      return {data: "Hello from Firebase"};
-    });
+  .region("asia-east2")
+  .https.onCall((payload, context) => {
+    // check if user is valid
+    if (!context.auth || !context.auth.token) {
+      throw new functions.https.HttpsError(
+        "permission-denied",
+        "To continue, please sign in the application"
+      );
+    }
+    const user_id = context.auth.token.uid;
+
+    logger.info(`user ${user_id} has logged in`);
+    return { code: 200, message: "Successful test." };
+  });
+
+exports.getAllStandupEntries = functions
+  .region("asia-east2")
+  .https.onCall(async (payload, context) => {
+    // check if user is valid
+    if (!context.auth || !context.auth.token) {
+      throw new functions.https.HttpsError(
+        "permission-denied",
+        "To continue, please sign in the application"
+      );
+    }
+    const user_id = context.auth.token.uid;
+
+    const db = admin.firestore();
+    const entriesQuery = db.collection('standup_entries').where("user_id", "==", user_id);
+    const entriesRef = await entriesQuery.get();
+    let entries = []
+
+    entriesRef.forEach((doc) => {
+      entries.push(doc.data());
+      console.log(doc.data())
+    })
+
+
+    logger.info(`user ${user_id} has accessed their entries`);
+    return { code: 200, message: "Successful read.", data: entries };
+  });
+
+exports.getStandupEntry= functions
+  .region("asia-east2")
+  .https.onCall(async (payload, context) => {
+    // check if user is valid
+    if (!context.auth || !context.auth.token) {
+      throw new functions.https.HttpsError(
+        "permission-denied",
+        "To continue, please sign in the application"
+      );
+    }
+    const user_id = context.auth.token.uid;
+
+    const db = admin.firestore();
+    const entryQuery = db.collection('standup_entries').doc("E8MXaOyoakaIpyTTP62W");
+    const entryRef = await entryQuery.get();
+
+    if (!entryRef.exists) {
+      console.log('No such document!');
+    } else {
+      console.log('Document data: exists');
+    }
+
+    logger.info(`user ${user_id} has accessed their entries`);
+    return { code: 200, message: "Successful read.", data: entryRef.data() };
+  });
+
+exports.createEntry = functions
+  .region("asia-east2")
+  .https.onCall(async (payload, context) => {
+    // check if user is valid
+    if (!context.auth || !context.auth.token) {
+      throw new functions.https.HttpsError(
+        "permission-denied",
+        "To continue, please sign in the application"
+      );
+    }
+    const user_id = context.auth.token.uid;
+    logger.info(`user_id: ${user_id} is creating an entry`);
+
+    const standup = {
+      did: "test",
+      do: "test",
+      blocker: "test",
+    };
+    const timestamp = Date.now();
+    try {
+      const write = await admin
+        .firestore()
+        .collection("standup_entries")
+        .add({ standup: standup, timestamp: timestamp, user_id: user_id });
+      logger.info({ result: `Message with ID: ${write.id} added.` });
+      return { code: 200, message: "Successful create." };
+    } catch (err) {
+      logger.error({
+        code: err.code,
+        message: err.message,
+        details: err.details,
+      });
+      return { code: err.code, message: err.message, details: err.details };
+    }
+  });
