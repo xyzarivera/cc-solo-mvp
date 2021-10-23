d94aeb4bf3102c0d1d249725cbabaab4d549fba1
Xyza Rivera <xyzarivera@gmail.com>
2021-10-23 12:01:30 +0800

    display timestamp, fix useEffect warning

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -3,20 +3,20 @@ import { useEffect, useState } from "react";
 import { getFunctions, httpsCallable } from "firebase/functions";
 import { getApp } from "firebase/app";
 import { useParams } from "react-router-dom";
+import moment from "moment";
 
 const Entry = () => {
   let { id } = useParams();
   const [standup, setStandup] = useState([]);
   const [timestamp, setTimestamp] = useState([]);
-  const [entry, setEntry] = useState([]);
   const [request_state, setRequestState] = useState("idle");
 
-  const read = async () => {
+  const read = async (id) => {
     setRequestState("pending");
     try {
       const functions = getFunctions(getApp(), "asia-east2");
       const getStandupEntry = httpsCallable(functions, "getStandupEntry");
-      const response = await getStandupEntry({ id });
+      const response = await getStandupEntry({id});
       console.log("getStandupEntry", response.data);
       setStandup(response.data.data.standup);
       setTimestamp(response.data.standup);
@@ -28,8 +28,8 @@ const Entry = () => {
   };
 
   useEffect(() => {
-    read();
-  }, []);
+    read(id);
+  }, [id]);
 
   return (
     <>
@@ -37,20 +37,25 @@ const Entry = () => {
       {request_state === "pending" ? "loading" : ""}
       {request_state === "rejected" ? "please refresh" : ""}
       {request_state === "fulfilled" ? (
-      <div>
         <div>
-          <p>What did you do yesterday?</p>
-          <p>{standup.did}</p>
+          <div>
+            <p>{`Entry date: ${moment(timestamp)
+              .format("MMM DD, YYYY hh:mm A")
+              .toString()}`}</p>
+          </div>
+          <div>
+            <p>What did you do yesterday?</p>
+            <p>{standup.did}</p>
+          </div>
+          <div>
+            <p>What will you do today?</p>
+            <p>{standup.do}</p>
+          </div>
+          <div>
+            <p>What (if anything) is blocking your progress?</p>
+            <p>{standup.blocker}</p>
+          </div>
         </div>
-        <div>
-          <p>What will you do today?</p>
-          <p>{standup.do}</p>
-        </div>
-        <div>
-          <p>What (if anything) is blocking your progress?</p>
-          <p>{standup.blocker}</p>
-        </div>
-      </div>
       ) : null}
     </>
   );
